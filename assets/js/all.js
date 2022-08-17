3518b67ef0f8f4b7375ab96d13fd93cb0d5c5797
HeroNeverDie072 <shadow820702@gmail.com>
2022-08-18 00:19:20 +0800

    Update 2022-08-17T16:19:18.801Z

---
+++ assets/js/all.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var swiper = new Swiper('.swiper', {
+  spaceBetween: 24,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".pagination-arrow-next",
+    prevEl: ".pagination-arrow-prev"
+  }
+});
+//# sourceMappingURL=all.js.map
