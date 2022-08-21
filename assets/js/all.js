88de8d9370545322e29f552a2247474688fdf0da
HeroNeverDie072 <shadow820702@gmail.com>
2022-08-21 20:50:25 +0800

    Update 2022-08-21T12:50:23.711Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -11,4 +11,8 @@ var swiper = new Swiper('.swiper', {
     prevEl: ".pagination-arrow-prev"
   }
 });
+var dateSelect = document.querySelector('input[name="dateSelect"]');
+var datepicker = new Datepicker(dateSelect, {
+  buttonClass: 'btn'
+});
 //# sourceMappingURL=all.js.map
